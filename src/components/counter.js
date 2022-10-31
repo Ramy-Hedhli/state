44cd0d69403c0c4cb798a1f3df661a40958dd33d
Ramy-Hedhli <Rmhedhly@gmail.com>
2022-10-31 14:11:05 +0100

    first

---
+++ src/components/counter.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+export class Counter extends React.Component{
+    constructor(){
+        super()
+        this.state={
+            count : 0,
+            textP : ''
+        }
+    }
+
+    componentDidMount(){
+        setInterval(() => {
+            this.setState({...this.state,count : this.state.count+1})
+        }, 1000);
+
+        
+    }
+    render(){
+        return(
+            <div>
+                <h1>{this.state.count}</h1>
+                <h2>Name : Hedhli Ramy</h2>
+                <img style={{width:"500px", margin:"30px"}} src='./Image/img.jpg' alt='Not Found' />
+                <h2>Bio :</h2>
+                <br/>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua</p>
+                <br/>
+                <h2>Profession : Student</h2>                
+            </div>
+        )
+    }
+}
